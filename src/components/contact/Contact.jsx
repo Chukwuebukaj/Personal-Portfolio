1b007166d6f5846b69ed39d377d256bbcceca5ff
Chukwuebukaj <anunihuebuka01@gmail.com>
2022-10-21 13:18:22 +0100

    I added the files to the repo

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_fvo5na8",
+      "template_s5r6h9b",
+      form.current,
+      "n966BZTUvanGXPnwV"
+    );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon email" />
+            <h4>Email</h4>
+            <h5>anunihuebuka01@gmail.com</h5>
+            <a href="mailto:anunihuebuka01@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon messenger" />
+            <h4>Messenger</h4>
+            <h5>Mayjay_e</h5>
+            <a href="https://m.me/mayjay.ebuks/" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option whatsapp">
+            <BsWhatsapp className="contact__option-icon " />
+            <h4>Whatsapp</h4>
+            <h5>+123456789</h5>
+            <a href="https://wa.me/2347064527012" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="text" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            cols="30"
+            rows="7"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/image1.svg";
+import IMG2 from "../../assets/cardio-class.jpg";
+import IMG3 from "../../assets/todo.jpeg";
+import IMG4 from "../../assets/tip-calc.jpg";
+import IMG5 from "../../assets/pexels5.jpg";
+import IMG6 from "../../assets/pexels6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "FoodBag: A food ordering website",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://my-restaurant-psi.vercel.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Health-fit: MentalHealth and Physical Fitness",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://health-fit.vercel.app/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Todo App: Track your daily activities",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://to-do-app-two-theta.vercel.app/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Tip Calculator",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://tip-calculator-ten-blond.vercel.app/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com/Chukwuebukaj",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
